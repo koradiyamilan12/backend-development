bc4b6e1815967991663b0250189764b6332a0404
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-02-03 18:59:44 +0530

    hello world node

---
+++ part-1/1.hello-world-node/index.js
@@ -0,0 +1,11 @@
+console.log("Hello js");
+
+const array = [1, 2, 3, 4];
+
+console.log(array, "array");
+
+setTimeout(() => {
+  console.log("This message is delayed by 2 secounds");
+}, 2000);
+
+console.log("This is the last line of the sync code");
